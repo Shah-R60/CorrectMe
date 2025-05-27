7c50df8f53be41f04199361177c4d3e6b3621864
Shah Rahul <shahrahul3600@gmail.com>
2025-05-27 20:52:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get('/topic', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://correct-frontend.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
