d7657856997966f256d801fd61da12038f3067b6
Shah Rahul <shahrahul3600@gmail.com>
2025-05-27 22:05:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ app.get('/topic', (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://correct-frontend.vercel.app',
-    methods: ['GET', 'POST']
+    origin: "https://correct-frontend.vercel.app", // You can also use an array here
+    methods: ["GET", "POST"], // Optional: specify allowed methods
+    credentials: true // Optional: if you're sending cookies/auth headers
   }
 });
 
